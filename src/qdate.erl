ab75b1fb78870cba3b7cc7e603b293b85d330238
Spoowy <erwin.flaming@gmail.com>
2018-10-29 19:06:22 +0100

    Update qdate.erl

--- src/qdate.erl
+++ src/qdate.erl
@@ -1076,7 +1076,7 @@ try_parsers(RawDate,[{ParserKey,Parser}|Parsers]) ->
         Other ->
             throw({invalid_parser_return_value,[{parser_key,ParserKey},{return,Other}]})
     catch
-        Error:Reason:Stachtrace -> 
+        Error:Reason:Stacktrace -> 
             throw({error_in_parser,[{error,{Error,Reason}},{parser_key,ParserKey}, {stacktrace, Stacktrace}]})
     end.
 
